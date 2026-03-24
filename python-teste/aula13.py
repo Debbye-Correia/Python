8f0f5cb6fd1d07181acd3724aed3dbadaca31462
DeboraCorreia <deboradasilvacorreia@outlook.com>
2026-03-24 16:31:15 +0100

    aula 13

---
+++ python-teste/aula13.py
@@ -0,0 +1,12 @@
+for c in range(1,6): #O ultimo numero ele nao considera, entao vai escrever apenas 5
+    print(c)
+print('Fim')
+print('=-' * 20)
+for c in range (6, 0, -1): #Para contagem regressiva precisa indicar a iteração com negativo na frente!
+    print(c)
+print('Fim!')
+print('=-' * 20)
+for c in range (0, 7, 2): #contagem pulando de 2 em 2
+    print(c)
+print('Fim!')
+print('=-' * 20)
