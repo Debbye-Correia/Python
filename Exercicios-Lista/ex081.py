bbf05bacb2bd4d6211b2851fd268906ad6399d79
DeboraCorreia <deboradasilvacorreia@outlook.com>
2026-05-02 11:38:23 +0200

    correcao ex 81 a 83

--- Exercicios-Lista/ex081.py
+++ Exercicios-Lista/ex081.py
@@ -1,13 +1,11 @@
 elementos = []
-c = 0
 while True:
     elementos.append(int(input('Digite um valor: ')))
-    c += 1
     r = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
     if r == 'N':
         break
 print('=-'*40)
-print(f'Você digitou {c} elementos.')
+print(f'Você digitou {len(elementos)} elementos.')
 elementos.sort(reverse=True)
 print(f'Os valores em ordem decrescente são {elementos}')
 if 5 in elementos:
--- Exercicios-Lista/ex083.py
+++ Exercicios-Lista/ex083.py
@@ -1,7 +1,16 @@
 expressao = str(input('Digite a expressão: '))
-
-if len('(') == len(')'):
-    print('Sua expressão está correta!')
+pilha = []
+for simbolo in expressao:
+    if simbolo == '(': # Cada vez que eu abro um parentese abrindo ele vai add na pilha
+        pilha.append('(')
+    elif simbolo == ')':  # e quando eu coloco um parentese fechando
+        if len(pilha) > 0: # ele vai remover um parentese da pilha (encontrando seu par)
+            pilha.pop()
+        else:
+            pilha.append(')') # se a pilha estiver vazia, ele vai add um parentese fechando e dar break
+            break             # Significando que a pilha nao esta vazia, e dando o erro
+if len(pilha) == 0:  #Vai significar que cada parentese que abriu tbem fechou
+    print('Sua expressão está valida!')
 else:
-    print('Sua expressão está errada!')
+    print('Sua expressão está incorreta!')
 
