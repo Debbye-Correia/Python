7fa6852349aac35eda13077e487aac5d27f41183
DeboraCorreia <deboradasilvacorreia@outlook.com>
2026-09-12 18:54:17 +0200

    correção ex88

--- Exercicios-Lista/ex088.py
+++ Exercicios-Lista/ex088.py
@@ -1,4 +1,4 @@
-jogo = []
+jogos = list()
 lista = list()
 from random import randint
 from time import sleep
@@ -6,17 +6,25 @@ print('-' * 40)
 print(f'{'JOGOS DA MEGA SENA':^40}')
 print('-' * 40)
 qtd = int(input('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= qtd:
+    cont = 0
+    while True:
+        num = randint(1,60)
+        if num not in lista:
+            lista.append(num)
+            cont+=1
+        if cont >=6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
 print(f'{'-='*5} SORTEANDO {qtd} JOGOS {'-='*5}')
-for q in range(qtd):
-    for n in range(0,6):
-        v = randint(1,60)
-        jogo.append(v)
-    lista.append(jogo[:])
-    jogo.clear()
-#sleep(2)
-for j in range(lista):
-    print(f'Jogo {j+1}: {lista[j]} ', end='')
-   # sleep(2)
+sleep(0.5)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    sleep(1)
 print()
 print(f'{'-='*5} < BOA SORTE! > {'-='*5}')
 
