e9c0b2ed3f2810707d1a0e439b208dbe1058a7f2
DeboraCorreia <deboradasilvacorreia@outlook.com>
2026-09-17 16:07:37 +0200

    pequenas correcoes no ex089

--- Exercicios-Lista/ex089.py
+++ Exercicios-Lista/ex089.py
@@ -1,9 +1,7 @@
-# lista = [[nome],[[n1],[n2]],[med]]
 aluno = list()
 nota = list()
 boletim = list()
 from time import sleep
-
 print('-='*5, 'CADASTRO ALUNOS', '-='*5)
 while True:
     aluno.append(str(input('Nome: ')))
@@ -21,17 +19,15 @@ print('-='*25)
 print(f'{'No.':<4} {'NOME':<13} {'MÉDIA'}')
 print('-'*30)
 for pos, a in enumerate(boletim):
-    print(f'{pos:<4} {boletim[pos][0]:<13}  {boletim[pos][2]:.1f}')
+    print(f'{pos:<4} {[a][0]:<13}  {[a][2]:>5.1f}')
 print('-'*30)
 while True:
     No = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
     if No == 999:
         print('FINALIZANDO...')
         sleep(1)
-        print('<<< VOLTE SEMPRE >>>')
         break
     else:
         print(f'As notas de {boletim[No][0]} são {boletim[No][1]}')
         print('-' * 50)
-
-
+print('<<< VOLTE SEMPRE >>>')
